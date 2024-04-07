acf45eab84bac20bd6df48baac5784099ec1670c
Samir <lehmn@bk.ru>
2024-04-08 00:16:58 +0600

    lab7

--- lab7/assignment/2.py
+++ lab7/assignment/2.py
@@ -1,9 +1,39 @@
 import pygame
+import os
+
 pygame.init()
 
+import os
+screen = pygame.display.set_mode((800, 600))
+def design():
+    COLOR1 = (186, 85, 211)
+    COLOR2 = (75, 0, 130)
+    playing_button = pygame.image.load(r"C:\Users\Самир\Desktop\pp2\lab7\images\play-pause-button.png")
+    playing_button = pygame.transform.scale(playing_button, (300, 80))
+    f1 = pygame.font.SysFont("Arial", 32)
+   
+    for x in range(800):
+        # Интерполяция цветов для каждой строки экрана
+        color = pygame.Color(
+            int(COLOR1[0] + (COLOR2[0] - COLOR1[0]) * (x / 800)),
+            int(COLOR1[1] + (COLOR2[1] - COLOR1[1]) * (x / 800)),
+            int(COLOR1[2] + (COLOR2[2] - COLOR1[2]) * (x / 800))
+        )
+        pygame.draw.line(screen, color, (x, 0), (x, 600))
+
+    for x in range(350):
+        color = pygame.Color(
+            int(COLOR2[0] + (COLOR1[0] - COLOR2[0]) * (x / 300)),
+            int(COLOR2[1] + (COLOR1[1] - COLOR2[1]) * (x / 300)),
+            int(COLOR2[2] + (COLOR1[2] - COLOR2[2]) * (x / 300))
+        )
+        pygame.draw.line(screen, color, (x + 200, 112.5 ), (x + 200, 337.5))
+
+    screen.blit(playing_button, (220, 360))
+    
 SONG_END = pygame.USEREVENT +  1
 pygame.mixer.music.set_endevent(SONG_END)
-songs = ['song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3']
+songs = ['lab7/song/song2.mp3', 'lab7/song/song3.mp3', 'lab7/song/song4.mp3']
 curently = None
 next_song = 0
 
@@ -21,8 +51,10 @@ def play_music(next_song):
 
 play_music(next_song)
 flag_pause = True
-screen = pygame.display.set_mode((100, 100))
+
 while True:
+    design()
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
